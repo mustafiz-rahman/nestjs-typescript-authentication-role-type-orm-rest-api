dbc7be11e1c993c92fab5d788994aeb56ebdb1d9
mustafiz-rahman <mustafiz.iub@gmail.com>
2021-11-10 00:26:57 +0600

    delete data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,37 @@
-import {Body, Controller,Delete,Get,Param,Patch,Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
-import { UpdateUserDto } from './dtos/update-user.dto'
-
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
-    constructor(private userService:UsersService){}
-    @Post('/signup')
-    createUser(@Body() body:CreateUserDto){
-        this.userService.create(body.email,body.password);
-    }
-    @Get()
-    findAll(){
-       return this.userService.find();
-    }
-    @Get('/:id')
-    findOne(@Param('id') id:string){
-        return this.userService.findOne(parseInt(id));
-    }
-    @Patch('/:id')
-    update(@Param('id') id:string,@Body() body:UpdateUserDto){
-        return this.userService.update(parseInt(id),body);
-    }
+  constructor(private userService: UsersService) {}
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.userService.create(body.email, body.password);
+  }
+  @Get()
+  findAll() {
+    return this.userService.find();
+  }
+  @Get('/:id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+  @Patch('/:id')
+  update(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.update(parseInt(id), body);
+  }
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,30 +3,34 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
-
-
 @Injectable()
 export class UsersService {
-    
-    constructor(@InjectRepository(User) private repo:Repository<User>){ }
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-    create(email:string,password:string){
-        const user = this.repo.create({email,password});
-        return this.repo.save(user);
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+  find() {
+    return this.repo.find();
+  }
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+  async update(id: number, atters: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
     }
-    find(){
-        
-        return this.repo.find();
+    Object.assign(user, atters);
+    return this.repo.save(user);
+  }
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
     }
-    findOne(id:number){
-        return this.repo.findOne(id);
-    }
-    async update(id:number,atters:Partial<User>){
-        const user = await this.findOne(id);
-        if(!user){
-            throw new Error('User not found'); 
-        }
-        Object.assign(user,atters);
-        return this.repo.save(user);
-    }
+
+    return this.repo.remove(user);
+  }
 }
